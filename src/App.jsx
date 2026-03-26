c53c29c0e3c663df3f4865c75776f1805618162b
aakritichoudhary <aakriti1805@gmail.com>
2026-03-26 20:20:22 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import { Analytics } from '@vercel/analytics/next';
 function App() {
   return (
     <div className="w-full">
@@ -15,6 +16,7 @@ function App() {
         <Experience />
         <Projects />
         <Footer />
+        <Analytics />
       </div>
     </div>
   );
